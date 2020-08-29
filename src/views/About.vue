1a91d72d6fb91cd30c7ec1ed1be65f4fb738e2ba
kremazar <kmazar222@gmail.com>
2020-08-29 16:23:37 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="jumbotron">
-  <h1 class="display-4">Dobro došli u aplikaciju TopDoktor</h1>
-  <hr class="my-4">
-  <h3  class="display-5">Pronađite i ocjenite vašeg doktora: </h3>
-  <br>
-  <router-link to="/doktori" tag="button">Doktori</router-link>
-
-</div>
- 
+    <h1 class="display-4">Dobro došli u aplikaciju TopDoktor</h1>
+    <hr class="my-4" />
+    <h3 class="display-5">Pronađite i ocjenite vašeg doktora:</h3>
+    <br />
+    <div v-if="islogged">
+      <router-link to="/doktori" tag="button">Doktori</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Doktori',
+  name: "Doktori",
   data() {
     return {
-      
+      islogged: localStorage.getItem("usertoken"),
     };
   },
-  methods: {
-   
-  },
+  methods: {},
   mounted() {
-     console.log(localStorage.getItem('usertoken'))
+    console.log(localStorage.getItem("usertoken"));
   },
 };
 </script>
