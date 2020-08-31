55da63a6dc87d96192e481fdb1423309dc03b20f
kremazar <kmazar222@gmail.com>
2020-08-31 03:12:51 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,9 @@
   <div class="jumbotron">
     <h1 class="display-4">Dobro došli u aplikaciju TopDoktor</h1>
     <hr class="my-4" />
-    <h3 class="display-5">Pronađite i ocjenite vašeg doktora:</h3>
-    <br />
     <div v-if="islogged">
+      <h3 class="display-5">Pronađite i ocjenite vašeg doktora:</h3>
+      <br />
       <router-link to="/doktori" tag="button">Doktori</router-link>
     </div>
   </div>
